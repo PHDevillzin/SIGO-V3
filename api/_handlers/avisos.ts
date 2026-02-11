8e797903b11ae59ae0c95a3d82d18ca9569bf7c2
PHDevillzin <paulo.ribeiro.3@sesisenaisp.org.br>
2026-02-11 08:06:25 -0300

    feat: Commit automatico

--- api/_handlers/avisos.ts
+++ api/_handlers/avisos.ts
@@ -1,6 +1,6 @@
 
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { query } from './db';
+import { query } from './db.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.setHeader('Access-Control-Allow-Credentials', 'true');
